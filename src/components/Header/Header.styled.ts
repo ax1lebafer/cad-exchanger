6f6f1ac5e310ffda59b0a3239662c4ebacf2c415
ax1lebafer <raul8917@gmail.com>
2024-10-29 22:35:23 +0400

    Декомпозировал Button.tsx

--- src/components/Header/Header.styled.ts
+++ src/components/Header/Header.styled.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { AppBar, Button } from "@mui/material";
+import { AppBar } from "@mui/material";
 import { Link } from "react-router-dom";
 import { breakpoints } from "../../lib/breakpoints.ts";
 
@@ -22,19 +22,5 @@ export const Logo = styled(Link)`
 
   @media screen and (max-width: ${breakpoints.md}px) {
     font-size: 20px;
-  }
-`;
-
-export const ButtonStyled = styled(Button)`
-  background-color: #222020;
-  border-radius: 10px;
-  padding: 5px 50px;
-
-  &:hover {
-    background-color: #302f2f;
-  }
-
-  @media screen and (max-width: ${breakpoints.md}px) {
-    padding: 5px 25px;
   }
 `;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,12 @@
 import * as S from "./Header.styled";
+import Button from "../Button/Button.tsx";
 
 export default function Header() {
   return (
     <S.Header position="sticky">
       <S.Logo to="/">Some Company</S.Logo>
 
-      <S.ButtonStyled variant="contained" size="large">
-        Contact us
-      </S.ButtonStyled>
+      <Button title="Contact us" />
     </S.Header>
   );
 }
